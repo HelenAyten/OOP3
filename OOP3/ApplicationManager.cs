c880522dc40421f73da7fd4f8cb45e5d63458c0b
Helen <ayten@baysal.com>
2021-01-24 00:15:39 +0300

    Add project files.

---
+++ OOP3/ApplicationManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class ApplicationManager
+    {
+        //Method injection
+        public void ToApply(ICreditManager creditManager, List<ILoggerService> loggerServices) //Başvuru yap kredi tekil olsun. Loglama birden fazla olsun.
+        {
+            creditManager.Calculate();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+
+        }
+        public void MakeCreditPreNotificitaion(List<ICreditManager> credits) //Kredi ön bilgilendirmesi yap
+        {
+            foreach (var credi in credits)
+                {
+                credi.Calculate();
+                }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditManager needCreditManager = new NeedCreditManager();            
+            ICreditManager vehicleCreditManager = new VehicleCreditManager();            
+            ICreditManager housingCreditManager = new HousingCreditManager();
+            ICreditManager artisanCreditManager = new ArtisanCreditManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+
+            List<ILoggerService> loggers = new List<ILoggerService>() { new SmsLoggerService(), new FileLoggerService() };
+
+            ApplicationManager applicationManager = new ApplicationManager();
+           applicationManager.ToApply(vehicleCreditManager, loggers);
+
+            List<ICreditManager> credits = new List<ICreditManager>(){needCreditManager,vehicleCreditManager};
+            //applicationManager.MakeCreditPreNotificitaion(credits);
+        }
+    }
+}
